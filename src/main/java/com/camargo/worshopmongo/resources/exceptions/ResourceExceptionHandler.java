37232c933f1ae1f466fd104bd01b1853e12358db
lucasdecamargosilva <lucasdecamargo2015@gmail.com>
2020-11-02 23:15:55 -0300

    Insercao de usuario com POST

--- src/main/java/com/camargo/worshopmongo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/camargo/worshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -14,7 +14,7 @@ public class ResourceExceptionHandler {
 
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
--- src/main/java/com/camargo/worshopmongo/services/UserService.java
+++ src/main/java/com/camargo/worshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.camargo.worshopmongo.domain.User;
+import com.camargo.worshopmongo.dto.UserDTO;
 import com.camargo.worshopmongo.repository.UserRepository;
 import com.camargo.worshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
